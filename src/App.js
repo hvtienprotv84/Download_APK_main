22c5b638950f23fc94ac5f0457b33820ff2298ac
hvtienprotv84 <dmx031117@gmail.com>
2025-03-23 18:30:47 +0700

    push update thêm router dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router và Routes
 import LanguageSelector from './component/containers/LanguageSelector';
 import { LanguageProvider } from './component/containers/Language';
 import Explore from './component/Explore';
 import Footer from './component/Footer';
+import HistoryAPK from './component/HistoryAPK';  // Import trang HistoryAPK
 
 function App() {
   return (
     <LanguageProvider>
-      <div className="App">
-        <header className="App-header">
-          <LanguageSelector />
-          <Explore />
-          <Footer/>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <LanguageSelector />
+            {/* <Explore /> */}
+            <Footer />
+            <Routes>
+              {/* Cấu hình các tuyến đường */}
+              <Route path="/" element={<Explore />} />
+              <Route path="/HistoryAPK" element={<HistoryAPK />} />
+            </Routes>
+          </header>
+        </div>
+      </Router>
     </LanguageProvider>
   );
  }
